36ce8be47a4464d9d168a96fd58021a6ba485b0b
jshulk <vijaytyagi.vt@gmail.com>
2023-08-18 13:16:14 +0530

    Added wait_for_db command

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Adds x, y and returns the result. """
     return x + y
+
 
 def subtract(x, y):
     """ Subtract x from y and returns the result """
